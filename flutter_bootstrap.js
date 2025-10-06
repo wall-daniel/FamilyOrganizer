70aa3d81714b3af2e00f04a5b98ede8907f95f81
wall-daniel <wall-daniel@users.noreply.github.com>
2025-10-06 22:11:06 +0000

    deploy: f83edc76f5dc7e8ace0a468292462b83c19fd33e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "501179130"
+    serviceWorkerVersion: "1251289858"
   }
 });
